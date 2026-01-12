217eee13b12d4ccc56b89d5547fbae0a423928af
Laxman <laxmanhenricks94@gmail.com>
2026-01-13 00:37:02 +0530

    added expiration time in coookie

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -10,7 +10,7 @@ export const generateJsonWebToken = async (user, message, statusCode, res) => {
     .status(statusCode)
     .cookie('token', token, {
       httpOnly: true,
-      maxAge: process.env.COOKIE_EXPIRE,
+      maxAge: process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000, // hr,min,sec,ms
       samSite: 'strict',
       secure: process.env.NODE_ENV !== 'development' ? true : false,
     })
